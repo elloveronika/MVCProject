d09a4f9f100c7aa63e98bd424d36ffe99738aa6b
elloveronika <ello.veronika92@gmail.com>
2021-06-06 15:50:11 -0700

    added all routes

--- app.js
+++ app.js
@@ -7,15 +7,25 @@ app.use(logger('dev'));
 
 app.use(express.json());
 
-
+app.set('view engine', 'ejs')
 app
-    .route('/')
+    .route('/').get((request, response) => response.render('feed' , {title:"Root route"}))
     .get((request, response) => response.send('This is my app'))
     .post((req, res) => {
 
 app
     console.log(req.body);
     res.json({message: "message recieved"});
+
+app.post('/posts', (req, res) => {
+    posts.push(req.body.post)
+    res.redirect('/post')
+})
+
+app.route('/api').post((req, res) => {
+    console.log(req.body);
+    res.json({ message: "message recieved sis lul"})
+})
 
     })
 

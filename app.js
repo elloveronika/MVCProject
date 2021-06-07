7caed808e366fc6dd2057af4fec22301784cca2d
elloveronika <ello.veronika92@gmail.com>
2021-06-07 16:26:21 -0700

    added routes

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const logger = require('morgan')
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize({
+    'dialect': 'sqlite',
+    'storage': 'development.db'
+})
+
+const posts = ["This is the post I want", "This is also the post I want"]
 
 const app = express()//this tells us that express is a fucntion
 
@@ -8,25 +16,29 @@ app.use(logger('dev'));
 app.use(express.json());
 
 app.set('view engine', 'ejs')
-app
-    .route('/').get((request, response) => response.render('feed' , {title:"Root route"}))
-    .get((request, response) => response.send('This is my app'))
-    .post((req, res) => {
 
-app
-    console.log(req.body);
-    res.json({message: "message recieved"});
+app.route('/').get((request, response) => response.render('feed' , {title:"Root route"}))
+
+
+app.get('/posts', (req,res) => {
+    res.render('../post.ejs', {
+    posts,
+    })
+})
+    
+// app
+//     // console.log(req.body);
+//     res.json({message: "message recieved"});
 
 app.post('/posts', (req, res) => {
     posts.push(req.body.post)
-    res.redirect('/post')
+    res.status(201).send();// changes the status code of the http response
 })
 
 app.route('/api').post((req, res) => {
     console.log(req.body);
-    res.json({ message: "message recieved sis lul"})
+    res.json({ message: "message recieved sis"})
 })
 
-    })
 
 app.listen(3000, () => console.log('Running on port 3000'))
